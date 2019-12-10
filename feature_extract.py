d1e54bdb95c2b34ad9dacde810b75d84c2d27d94
saranpong <creadz@gmail.com>
2019-12-10 16:29:59 +0700

    add face_regconition_image, update trainset

--- feature_extract.py
+++ feature_extract.py
@@ -13,16 +13,16 @@ FACE_DESC = []
 FACE_NAME = []
 
 for folder in os.listdir(path):
-   for fn in os.listdir(path + folder):
+   for fn in os.listdir(os.path.join(path, folder)):
       if fn.endswith('.jpg'):
-         img = cv2.imread(path + folder + '/' + fn)[:,:,::-1]
+         img = cv2.imread(os.path.join(path, folder, fn))[:,:,::-1]
          dets = detector(img, 1)
          for k, d in enumerate(dets):
             shape = sp(img, d)
-            face_desc = model.compute_face_descriptor(img, shape, 1)
+            face_desc = model.compute_face_descriptor(img, shape, 100)
             FACE_DESC.append(np.array(face_desc))
-            print('loading...', fn)
             FACE_NAME.append(folder)
+            print('loading...', folder, 'filename:', fn)
 
 pickle.dump((FACE_DESC, FACE_NAME), open('trainset.pk', 'wb'))
 
